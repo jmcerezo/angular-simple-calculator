c981853c4f8b75744fc5ce1d2ce7e7c49068fa17
JM Cerezo <johnmenardccerezo@gmail.com>
2024-01-30 23:48:59 +0800

    Move code to root component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-simple-calculator' title`, () => {
+  it(`should have the 'Angular Simple Calculator' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-simple-calculator');
+    expect(app.title).toEqual('Angular Simple Calculator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-simple-calculator');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Angular Simple Calculator'
+    );
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,125 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CalculatorComponent } from './components/calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CalculatorComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'Angular Simple Calculator';
+  displayValue: string = '0';
+  firstOperand: number | null = null;
+  secondOperand: number | null = null;
+  operator: string | null = null;
+  waitingForSecondOperand: boolean = false;
+
+  onClearClick(): void {
+    this.displayValue = '0';
+    this.firstOperand = null;
+    this.secondOperand = null;
+    this.operator = null;
+    this.waitingForSecondOperand = false;
+  }
+
+  onDecimalClick(): void {
+    if (!this.displayValue.includes('.') && !this.displayValueIsNaN()) {
+      this.displayValue += '.';
+    }
+  }
+
+  onDeleteClick(): void {
+    if (this.displayValueIsNaN()) return;
+
+    if (this.displayValue.length > 1) {
+      this.displayValue = this.displayValue.slice(0, -1);
+    } else {
+      this.displayValue = '0';
+    }
+  }
+
+  onDigitClick(digit: string): void {
+    if (this.displayValueIsNaN()) return;
+
+    if (this.waitingForSecondOperand) {
+      this.displayValue = digit;
+      this.waitingForSecondOperand = false;
+    } else {
+      const value = this.displayValue + digit;
+      this.displayValue = this.displayValue === '0' ? digit : value;
+    }
+  }
+
+  onEqualClick(): void {
+    if (this.operator !== null) {
+      this.calculate();
+    }
+
+    this.operator = null;
+  }
+
+  onNegativeClick() {
+    if (this.displayValueIsNaN() || this.displayValue === '0') return;
+
+    this.displayValue = this.displayValue.includes('-')
+      ? this.displayValue.slice(1)
+      : '-' + this.displayValue;
+  }
+
+  onOperatorClick(operator: string): void {
+    if (this.displayValueIsNaN()) return;
+
+    if (this.firstOperand === null) {
+      this.firstOperand = Number(this.displayValue);
+    } else {
+      this.calculate();
+    }
+
+    this.operator = operator;
+    this.waitingForSecondOperand = true;
+  }
+
+  private displayValueIsNaN(): boolean {
+    return isNaN(Number(this.displayValue));
+  }
+
+  private displayErrorAndResetValues(): void {
+    this.displayValue = 'Error: Division by zero';
+    this.firstOperand = null;
+    this.secondOperand = null;
+    this.operator = null;
+    this.waitingForSecondOperand = false;
+  }
+
+  private calculate(): void {
+    if (this.firstOperand === null) return;
+
+    this.secondOperand = Number(this.displayValue);
+    let result: number;
+
+    switch (this.operator) {
+      case '+':
+        result = this.firstOperand + this.secondOperand;
+        break;
+      case '-':
+        result = this.firstOperand - this.secondOperand;
+        break;
+      case '*':
+        result = this.firstOperand * this.secondOperand;
+        break;
+      case '/':
+        if (this.secondOperand === 0) this.displayErrorAndResetValues();
+        result = this.firstOperand / this.secondOperand;
+        break;
+      default:
+        return;
+    }
+
+    if (!isNaN(result)) {
+      this.displayValue = '0';
+      this.firstOperand = result;
+    }
+  }
+}
